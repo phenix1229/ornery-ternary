91dc689471897b2cb8b4bf59d71b11849cc0bf9a
phenix1229 <paul.garay@codeimmersives.com>
2019-10-02 15:03:09 -0400

    completed all tasks

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-function isHot() {
-
+function isHot(temp) {
+  const reply = temp >= 80 ? 'Yes, it is indeed hot.' : 'No, it is not hot.';
+  return reply;
 }
 
 
-function helloThere() {
-
+function helloThere(name) {
+  const greeting = name.length > 5  ? 'Hi, ' + name : 'Hello, ' + name;
+  return greeting;
 }
 
-
-function goodbyeYou() {
-
+console.log(reply)
+function goodbyeYou(name) {
+  reply = name !== undefined ? 'Goodbye, ' + name : 'Goodbye, stranger';
+  return reply;
 }
 
 
